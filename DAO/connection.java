73e7bb4ddec88d06fd8a91d2c841005ea5ea0913
sipra7811 <139745826+sipra7811@users.noreply.github.com>
2024-03-19 16:18:10 +0530

    Add files via upload

---
+++ DAO/connection.java
@@ -0,0 +1,18 @@
+import java.sql.*;  
+
+public class connection{
+    Connection c;
+    Statement s;
+    public connection(){  
+        try{  
+            Class.forName("com.mysql.cj.jdbc.Driver");  
+            c =DriverManager.getConnection("jdbc:mysql:///HOSPITAL","root","sipra@1234");    
+            s =c.createStatement(); 
+           
+          
+            
+        }catch(Exception e){ 
+            System.out.println(e);
+        }  
+    }  
+}
